76ab2adbbcfb85dfd14a46651eccb842159ad606
ben black <weepingwillowben@gmail.com>
2018-04-28 00:48:57 -0700

    added second layer

--- process_sound.py
+++ process_sound.py
@@ -9,39 +9,46 @@ from shared_save import RememberSharedVals
 from WeightBias import WeightBias
 theano.config.optimizer="fast_compile"
 NUM_LOOK_BACK = 20
-plotutil = plot_utility.PlotHolder("basic_testa")
-shared_value_saver = RememberSharedVals('basic_weights')
+plotutil = plot_utility.PlotHolder("two_layers")
+shared_value_saver = RememberSharedVals('two_layers')
 
 def square(x):
     return x * x
 
-def get_pred_train_fns(inlen, outlen, hiddenlen, train_update_const):
+def get_pred_train_fns(inlen, outlen, hiddenlen1, hiddenlen2, train_update_const):
     inputvec = T.vector('invec', dtype=theano.config.floatX)
     expectedvec = T.vector('expected', dtype=theano.config.floatX)
 
-    hiddenfn = WeightBias("hidden",inlen, hiddenlen)
-    outfn = WeightBias("out",hiddenlen, outlen)
+    hiddenfn1 = WeightBias("hidden1",inlen, hiddenlen1)
+    hiddenfn2 = WeightBias("hidden2",hiddenlen1, hiddenlen2)
+    outfn = WeightBias("out",hiddenlen2, outlen)
 
-    shared_value_saver.add_shared_vals(hiddenfn.wb_list())
+    shared_value_saver.add_shared_vals(hiddenfn1.wb_list())
+    shared_value_saver.add_shared_vals(hiddenfn2.wb_list())
     shared_value_saver.add_shared_vals(outfn.wb_list())
 
     def logistic(x):
         one = np.float32(1.0)
         return one / (one+T.exp(-x))
 
-    hidvec = logistic(hiddenfn.calc_output(inputvec))
-    outvec = logistic(outfn.calc_output(hidvec))
+    hidvec1 = logistic(hiddenfn1.calc_output(inputvec))
+    hidvec2 = logistic(hiddenfn2.calc_output(hidvec1))
+    outvec = logistic(outfn.calc_output(hidvec2))
 
     actual = outvec
     diff = square(expectedvec - actual)
     error = diff.sum()
 
-    outdiff = outfn.update(error,np.float32(train_update_const))
-    hiddiff = hiddenfn.update(error,np.float32(train_update_const))
+    all_updates = (
+        outfn.update(error,np.float32(train_update_const)) +
+        hiddenfn1.update(error,np.float32(train_update_const)) +
+        hiddenfn2.update(error,np.float32(train_update_const))
+    )
 
-    hidbias_plot = plotutil.add_plot("hidbias",hiddenfn.b,1000)
-    outbias_plot = plotutil.add_plot("outbias",outfn.b,1000)
-    outbias_plot = plotutil.add_plot("error",error,100)
+    plotutil.add_plot("hidweight1",hiddenfn1.W,1000)
+    plotutil.add_plot("hidweight2",hiddenfn2.W,1000)
+    plotutil.add_plot("outweight",outfn.W,1000)
+    plotutil.add_plot("error",error,100)
 
     predict = theano.function(
             inputs=[inputvec],
@@ -51,7 +58,7 @@ def get_pred_train_fns(inlen, outlen, hiddenlen, train_update_const):
     train = theano.function(
             inputs=[inputvec,expectedvec],
             outputs=plotutil.append_plot_outputs([]),
-            updates=hiddiff+outdiff,
+            updates=all_updates,
         )
     saved_train_fn = shared_value_saver.share_save_fn(train)
     plotted_train_fn = plotutil.get_plot_update_fn(saved_train_fn)
@@ -84,6 +91,6 @@ def predict(sig, samplerate, my_predict_fn):
 sig, samplerate = sf.read('output.wav')
 sig = sig.astype(np.float32)
 num_channels = sig.shape[1]
-pred_fn, train_fn = get_pred_train_fns(num_channels*NUM_LOOK_BACK,num_channels,30,0.1)
+pred_fn, train_fn = get_pred_train_fns(num_channels*NUM_LOOK_BACK,num_channels,30,30,0.01)
 train_on_data(sig, samplerate, train_fn)
 #predict(sig, samplerate, pred_fn)
