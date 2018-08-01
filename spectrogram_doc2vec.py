4b6a6c92483ffeb56afd64ba625c114f7a265e47
ben black <weepingwillowben@gmail.com>
2018-08-01 08:58:50 -0700

    removed old unecessary filesg

--- spectrogram_doc2vec.py
+++ spectrogram_doc2vec.py
@@ -6,7 +6,6 @@ import tensorflow.contrib.signal as tfsignal
 import tensorflow as tf
 import os
 from WeightBias import DenseLayer
-from vector_result_processing import ResultsTotal
 
 import process_fma_files
 from file_processing import mp3_to_raw_data
@@ -51,6 +50,19 @@ class OutputVectors:
 
     def get_vector_values(self,sess):
         return sess.run(self.all_vectors)
+
+class ResultsTotal:
+    def __init__(self,vectors_dir):
+        self.vectors_dir = vectors_dir
+
+    def get_filepath(self,timestep):
+        return "{path}vector_at_{timestep}.npy".format(path=self.vectors_dir,timestep=timestep)
+
+    def save_file(self,data,timestep):
+        np.save(self.get_filepath(timestep),data)
+
+    def clear_files(self):
+        shutil.rmtree(self.vectors_dir)
 
 def find_random_comparison(song_list):
     song_idx = random.randrange(len(song_list))
